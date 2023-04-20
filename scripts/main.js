90d2a3cc643a3769cf5e78335ee9c6293314dc12
luc205299 <lucasbarthelemy43@gmail.com>
2023-04-20 09:28:33 +0200

    MyFirstWeb

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+let MyButton = document.querySelector('#jjj')
+MyButton.addEventListener('click', function(){
+    window.open('https://www.youtube.com/watch?v=Jb392j1BL3U')
+})
+let MyButton1 = document.querySelector('#jjjj')
+MyButton1.addEventListener('click', function(){
+    window.open('https://www.youtube.com/watch?v=j5a0jTc9S10')
+})
